2f6f8dc81b19cdabf133138787259567b14e420d
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-07-02 17:40:06 +0300

    First Commit

---
+++ src/components/Listing.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ItemList from './ItemList.js';
+
+export default function Listing(props) {
+  const { items } = props;
+  // console.log('Listing: ', items);
+
+  if (!items.length) {
+    return null;
+  }
+
+  return (
+    <div className="item-list">
+      {items.map((item) => (item.state === 'removed' ? null : (
+        <div key={item.listing_id} className="item">
+          <ItemList item={item} />
+        </div>)))}
+    </div>
+  );
+}
+
+Listing.defaultProps = {
+  items: [],
+};
+
+Listing.propTypes = {
+  items: PropTypes.array,
+};
